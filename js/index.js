27267772dd0d6bbdbb5e9710171cff811a7e1ae8
martincmr1 <martincmr1@hotmail.com>
2023-02-24 19:30:35 -0300

    ArgentinaprogramaRugniaCarlosMartin

---
+++ js/index.js
@@ -0,0 +1,15 @@
+document.getElementById ('exp1').onclick= function(){
+    document.getElementById("exp1").innerHTML="Cargo:<b> Administrativo</b> (1998-1999)"
+}
+document.getElementById ('exp2').onclick= function(){
+    document.getElementById("exp2").innerHTML="Cargo:<b> Supervisor </b>(1999-2003)    "
+}
+document.getElementById ('exp3').onclick= function(){
+    document.getElementById("exp3").innerHTML="Cargo:<b> Data Entry </b>(2003-2010)"
+}
+document.getElementById ('exp4').onclick= function(){
+    document.getElementById("exp4").innerHTML="Cargo:<b> Productor</b> (2010-2020) "
+}
+document.getElementById ('exp5').onclick= function(){
+    document.getElementById("exp5").innerHTML="Cargo:<b>Chef</b>(2020-2023) "
+}
